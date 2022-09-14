bb2f0bfb820f738f1095316d0bbd9c880f2f9443
chenghsienyu <chenghsienyu@aim.com>
2022-09-14 16:59:19 +0800

    update

--- renderer.js
+++ renderer.js
@@ -7,14 +7,16 @@ const oscServer = new Server(3333, '0.0.0.0');
 
 let pyshell = new PythonShell('./main.py');
 let msg_arr = [];
-let profile_arr = [];
+let profile_arr_a = [];
+let profile_arr_b = [];
+let profile_arr_c = [];
 
 function printer_a_request() {
     axios
         .get('http://192.168.168.87:8090/print')
         .then(res => {
             console.log(res.data)
-            update_profile(res.data)
+            update_profile_a(res.data)
         })
         .catch(err => {
             console.log(err)
@@ -26,6 +28,7 @@ function printer_b_request() {
         .get('http://192.168.168.91:8090/print')
         .then(res => {
             console.log(res.data)
+            update_profile_b(res.data)
         })
         .catch(err => {
             console.log(err)
@@ -37,17 +40,33 @@ function printer_c_request() {
         .get('http://192.168.168.92:8090/print')
         .then(res => {
             console.log(res.data)
+            update_profile_c(res.data)
         })
         .catch(err => {
             console.log(err)
         })
 }
 
-function update_profile(data) {
-    profile_arr.push(data);
-    //console.log(profile_arr);
-    if (profile_arr.length > 3) {
-        profile_arr.shift();
+function update_profile_a(data) {
+    profile_arr_a.push(data);
+    console.log(profile_arr_a);
+    if (profile_arr_a.length > 1) {
+        profile_arr_a.shift();
+    }
+}
+
+function update_profile_b(data) {
+    profile_arr_b.push(data);
+    console.log(profile_arr_b);
+    if (profile_arr_b.length > 1) {
+        profile_arr_b.shift();
+    }
+}
+function update_profile_c(data) {
+    profile_arr_c.push(data);
+    console.log(profile_arr_c);
+    if (profile_arr_c.length > 1) {
+        profile_arr_c.shift();
     }
 }
 
@@ -76,38 +95,52 @@ function update_msg(msg) {
 
 
 function update_ui_a() {
-    let file_path_a = profile_arr[0].file_path;
+    let file_path_a = profile_arr_a[0].file_path;
     let temp_arr_a = file_path_a.split('/');
     let final_path_a = 'view' + '/' + temp_arr_a[4] + '/' + temp_arr_a[5];
     document.getElementsByClassName('pic_a')[0].innerHTML = `<img width="100" height="100" src=${final_path_a}>`;
-    document.getElementsByClassName('ip_address_a')[0].innerHTML = profile_arr[0].ip_address;
-    document.getElementsByClassName('name_a')[0].innerHTML = profile_arr[0].name;
-    document.getElementsByClassName('phone_a')[0].innerHTML = profile_arr[0].phone;
-    document.getElementsByClassName('email_a')[0].innerHTML = profile_arr[0].email;
+    document.getElementsByClassName('ip_address_a')[0].innerHTML = profile_arr_a[0].ip_address;
+    document.getElementsByClassName('name_a')[0].innerHTML = profile_arr_a[0].name;
+    document.getElementsByClassName('phone_a')[0].innerHTML = profile_arr_a[0].phone;
+    document.getElementsByClassName('email_a')[0].innerHTML = profile_arr_a[0].email;
+}
 
-    let file_path_b = profile_arr[1].file_path;
+function update_ui_b() {
+    let file_path_b = profile_arr_b[0].file_path;
     let temp_arr_b = file_path_b.split('/');
     let final_path_b = 'view' + '/' + temp_arr_b[4] + '/' + temp_arr_b[5];
     document.getElementsByClassName('pic_b')[0].innerHTML = `<img width="100" height="100" src=${final_path_b}>`;
-    document.getElementsByClassName('ip_address_b')[0].innerHTML = profile_arr[1].ip_address;
-    document.getElementsByClassName('name_b')[0].innerHTML = profile_arr[1].name;
-    document.getElementsByClassName('phone_b')[0].innerHTML = profile_arr[1].phone;
-    document.getElementsByClassName('email_b')[0].innerHTML = profile_arr[1].email;
+    document.getElementsByClassName('ip_address_b')[0].innerHTML = profile_arr_b[0].ip_address;
+    document.getElementsByClassName('name_b')[0].innerHTML = profile_arr_b[0].name;
+    document.getElementsByClassName('phone_b')[0].innerHTML = profile_arr_b[0].phone;
+    document.getElementsByClassName('email_b')[0].innerHTML = profile_arr_b[0].email;
+}
 
-    let file_path_c = profile_arr[2].file_path;
+function update_ui_c() {
+    let file_path_c = profile_arr_c[0].file_path;
     let temp_arr_c = file_path_c.split('/');
     let final_path_c = 'view' + '/' + temp_arr_c[4] + '/' + temp_arr_c[5];
     document.getElementsByClassName('pic_c')[0].innerHTML = `<img width="100" height="100" src=${final_path_c}>`;
-    document.getElementsByClassName('ip_address_c')[0].innerHTML = profile_arr[2].ip_address;
-    document.getElementsByClassName('name_c')[0].innerHTML = profile_arr[2].name;
-    document.getElementsByClassName('phone_c')[0].innerHTML = profile_arr[2].phone;
-    document.getElementsByClassName('email_c')[0].innerHTML = profile_arr[2].email;
+    document.getElementsByClassName('ip_address_c')[0].innerHTML = profile_arr_c[0].ip_address;
+    document.getElementsByClassName('name_c')[0].innerHTML = profile_arr_c[0].name;
+    document.getElementsByClassName('phone_c')[0].innerHTML = profile_arr_c[0].phone;
+    document.getElementsByClassName('email_c')[0].innerHTML = profile_arr_c[0].email;
 }
 
 oscServer.on('message', function (msg) {
     console.log(`Message: ${msg}`);
-    printer_a_request();
-    update_ui_a();
+    if (msg == '/printer_a') {
+        printer_a_request();
+        update_ui_a();
+    }
+    if (msg == '/printer_b') {
+        printer_b_request();
+        update_ui_b();
+    }
+    if (msg == '/printer_c') {
+        printer_c_request();
+        update_ui_c();
+    }
 });
 
 pyshell.on('message', function (message) {
